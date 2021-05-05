07ec162c464a1db8b360d0ef01901bd4867ec13c
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2021-05-06 05:43:29 +0700

    add: countries select box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide')
+
+  useEffect(() => {
+    const getCountriesData = () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map(country => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries)
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-      <h1>COVID 19 tracker</h1>
+      <div className="app__header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value={country}
+            onChange={event => setCountry(event.target.value)}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country, index) => (
+              <MenuItem value={country.value} key={index}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
